79ce7e52aa18fa90e3e38b8f807c7fa9a97a9a87
fabioo66 <torrejonfabio@gmail.com>
2024-05-06 17:24:30 -0300

    Corrijo compresor de imagenes

--- module/tools.py
+++ module/tools.py
@@ -5,18 +5,14 @@ import json
 
 
 def process_image(image_uploaded):
-    """
-    Process the uploaded image by removing its background using the rembg library.
-    """
+    """Process the uploaded image by removing its background using the rembg library."""
     image = Image.open(image_uploaded)
     processed_image = remove_background(image)
     return processed_image
 
 
 def remove_background(image):
-    """
-    Remove the background from the given image using the rembg library.
-    """
+    """Remove the background from the given image using the rembg library."""
     image_byte = io.BytesIO()
     image.save(image_byte, format="PNG")
     image_byte.seek(0)
@@ -24,15 +20,23 @@ def remove_background(image):
     return Image.open(io.BytesIO(proccesed_image_bytes))
 
 
-def compress_image(uploaded_image):
+def compress_image(uploaded_image, quality=50, max_size=(1024, 1024)):
     """
     Compress the uploaded image to reduce file size.
     """
-    picture = Image.open(uploaded_image)
-    picture.save("compressed_image.jpg", optimize=True, quality=60)
-    return picture
+    img = Image.open(uploaded_image)
+    img.thumbnail(max_size)
+    
+    img_byte_array = io.BytesIO()
+    img.save(img_byte_array, format='JPEG', optimize=True, quality=quality)
+    
+    img_byte_array.seek(0)
+    compressed_img = Image.open(img_byte_array)
+    
+    return compressed_img
 
 
 def load_lottieflie(filepath: str):
+    """This function creates the animation"""
     with open(filepath, "r") as f:
         return json.load(f)
--- pages/02_Comprimir_imagen.py
+++ pages/02_Comprimir_imagen.py
@@ -13,19 +13,14 @@ st.subheader("Adjuntar imagen")
 upload_image = st.file_uploader("Elegi una imagen...", type=[".jpg", ".jpeg", ".png"])
 
 if upload_image is not None:
-    initial_size = len(upload_image.getvalue())
 
     compress_button = st.button(label="Comprimir")
 
-    compressed_image = tools.compress_image(upload_image)
+    if compress_button:
+        compressed_image = tools.compress_image(upload_image)
 
-    final_size = len(compressed_image.tobytes())
+        compressed_image.save("download_images/compressed_image.png")
 
-    # Debo mejorar logica de unidades de tamaño
-    st.success(f"Imagen comprimida. Se redujo de {initial_size} a {final_size}")
-
-    compressed_image.save("download_images/compressed_image.png")
-
-    with open("download_images/compressed_image.png", "rb") as file:
-        image_data = file.read()
-    st.download_button("Descargar imagen comprimida", data=image_data, file_name="compressed_image.jpg")
+        with open("download_images/compressed_image.png", "rb") as file:
+            image_data = file.read()
+        st.download_button("Descargar imagen comprimida", data=image_data, file_name="compressed_image.jpg")
